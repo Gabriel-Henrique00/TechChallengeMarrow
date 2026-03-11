c3b7ee1d28b281f1ac8959cd4fc3d285f7a5bd91
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2026-03-10 21:26:04 -0300

    feat: adiciona enums de status de pagamento e tentativa

---
+++ charger/apps/backend/src/shared/enums/status-pagamento.enum.ts
@@ -0,0 +1,7 @@
+export enum StatusPagamento {
+    AGUARDANDO_PAGAMENTO = 'AGUARDANDO_PAGAMENTO',
+    PAGO                 = 'PAGO',
+    NAO_AUTORIZADO       = 'NAO_AUTORIZADO',
+    CANCELADO            = 'CANCELADO',
+    VENCIDO              = 'VENCIDO',
+}
---
+++ charger/apps/backend/src/shared/enums/status-tentativa.enum.ts
@@ -0,0 +1,6 @@
+export enum StatusTentativa {
+    PROCESSANDO    = 'PROCESSANDO',
+    SUCESSO        = 'SUCESSO',
+    FALHA          = 'FALHA',
+    NAO_AUTORIZADO = 'NAO_AUTORIZADO',
+}
